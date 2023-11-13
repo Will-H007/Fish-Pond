d53c7c865991ecec1e779dbb0019acc40dbb268f
Will-H007 <88605676+Will-H007@users.noreply.github.com>
2023-11-13 21:59:42 +0800

    added initial container and border

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+        <div className ="flex flex-row">
+
+        <div className="container mx-auto px-2 basis-1/2">
+          <div className="h-screen box-border w-30 p-4 border-4 border-gray-300 mx-auto hover:border-gray-400 transition">
+            <p className="text-white">sdadasds</p>
+          </div>
+        </div>
+
+
+          <div className="container mx-auto px-2 basis-1/2">
+          <div className="h-screen box-border w-30 p-4 border-4 border-gray-300 mx-auto hover:border-gray-400 transition">
+                <p className="text-white">sdadasds</p>
+              </div>
+          </div>
+
+  
+
+
+  
+
+        </div>
+</div>
+   
+  );
+}
