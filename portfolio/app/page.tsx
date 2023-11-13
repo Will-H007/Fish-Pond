cb90828fc165f661c587920d6738be67d677cb3f
Will-H007 <88605676+Will-H007@users.noreply.github.com>
2023-11-13 22:15:33 +0800

    Added profile picture

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -7,14 +7,19 @@ export default function Home() {
         <div className ="flex flex-row">
 
         <div className="container mx-auto px-2 basis-1/2">
-          <div className="h-screen box-border w-30 p-4 border-4 border-gray-300 mx-auto hover:border-gray-400 transition">
-            <p className="text-white">sdadasds</p>
+          <div className="h-screen box-border w-30 p-4 border-2 border-gray-300 mx-auto hover:border-gray-400 transition">
+
+            <img className="w-20 h-20 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="./photo.jpeg" alt="Bordered avatar"/>
+
+
+            <p className="text-white">William Ho</p>
+     
           </div>
         </div>
 
 
           <div className="container mx-auto px-2 basis-1/2">
-          <div className="h-screen box-border w-30 p-4 border-4 border-gray-300 mx-auto hover:border-gray-400 transition">
+              <div className="h-screen box-border w-30 p-4 border-2 border-gray-300 mx-auto hover:border-gray-400 transition">
                 <p className="text-white">sdadasds</p>
               </div>
           </div>
