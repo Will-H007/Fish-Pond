c197c4d0924c7750cd2cb10d0562fd925d90bc2d
Will Ho <wkayi076@gmail.com>
2023-12-02 15:27:58 +0800

    fixed position

--- portfolio/app/information/page.tsx
+++ portfolio/app/information/page.tsx
@@ -1,6 +1,6 @@
 // Import necessary dependencies
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import DoubleLinkedList from "@/component/two_pointers/Boxes";
 import GlassmorphismBox from '@/component/Second_page/glass_box';
 
@@ -12,32 +12,39 @@ const initialItems = [
   "#20B2AA", "#8A2BE2", "#FA8072", "#2E8B57", "#696969"
 ];
 
-// Define the DoubleLinkedListLayout component
+
+
 const DoubleLinkedListLayout: React.FC = () => {
+  // State to store visible items
+  const [visibleItems, setVisibleItems] = useState<string[]>([]);
+
   // Callback function to handle visible items change
-  const handleVisibleItemsChange = (visibleItems: string[]) => {
-    console.log('Visible Items:', visibleItems);
+  const handleVisibleItemsChange = (items: string[]) => {
+    console.log('Visible Items:', items);
+    // Update the state with the new visible items
+    setVisibleItems(items);
   };
 
   return (
-    <div className="h-screen flex gap-x-8 w-max-xl grid grid-rows-7 grid-cols-10 m-8 justify-center items-center">
+    <div className="h-screen flex gap-x-8 w-max-xl grid grid-rows-7 grid-cols-11 m-10 justify-center items-center">
 
+      <div className="flex flex-wrap col-start-7 col-end-12 row-start-1 row-end-7 m-2 justify-center items-end h-full w-full">
+      <div className={`m-4 bg-opacity-25 backdrop-blur-lg p-8 rounded-lg shadow-xl w-full h-full`} style={{ backgroundColor: visibleItems[Math.floor(visibleItems.length / 2)] }}>
+      <div className="text-gray-600">
+        Glassmorphism
+      </div>
+    </div>
 
-      <div className="flex flex-wrap col-start-1 col-end-4 row-start-1 row-end-7 m-2 justify-center items-end h-full w-full">
-          <GlassmorphismBox item={"asdasd"}/>
       </div>
 
-      <div className="flex flex-wrap col-start-4 col-end-7 row-start-1 row-end-7 m-2 justify-center items-end h-full w-full">
-          <GlassmorphismBox item={"asdasd"}/>
-      </div>
 
-      <div className="flex flex-wrap col-start-7 col-end-10 row-start-1 row-end-7 m-2 justify-center items-end h-full w-full">
-          <GlassmorphismBox item={"asdasd"}/>
-      </div>
-    
 
-      <div className="row-start-7 col-span-10 justify-center items-center mb-2">
-        <div className="flex flex-row h-1/2 gap-x-8 p-2 bg-opacity-25 backdrop-blur-lg justify-center items-center">
+
+
+
+      <div className="row-start-7 h-1/2 col-span-full justify-center items-center mb-2">
+        <div className="flex flex-row gap-x-8 p-2 bg-opacity-25 backdrop-blur-lg justify-center items-center">
+          {/* DoubleLinkedList component */}
           <DoubleLinkedList items={initialItems} visibleItemCount={3} onVisibleItemsChange={handleVisibleItemsChange} />
         </div>
       </div>
--- portfolio/component/Second_page/glass_box.tsx
+++ portfolio/component/Second_page/glass_box.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 interface GlassmorphismBoxProps {
   item: string | null;
+  color: string;
 }
 
-const GlassmorphismBox: React.FC<GlassmorphismBoxProps> = ({ item }) => {
+const GlassmorphismBox: React.FC<GlassmorphismBoxProps> = ({ item,color }) => {
   return (
-    <div className="m-4 bg-opacity-25 backdrop-blur-lg bg-white p-8 rounded-lg shadow-xl w-full h-full">
-      <h1 className="text-2xl font-bold mb-4 shrink">GlassMorphism box</h1>
+    <div className={`m-4 bg-opacity-25 backdrop-blur-lg bg-[${color}] p-8 rounded-lg shadow-xl w-full h-full`}>
       <div className="text-gray-600">
         {item}
       </div>
