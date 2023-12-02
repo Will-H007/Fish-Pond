7c25501107b8614e141151057aac2de0a6456694
Will Ho <wkayi076@gmail.com>
2023-12-02 17:17:47 +0800

    added card

--- portfolio/app/information/page.tsx
+++ portfolio/app/information/page.tsx
@@ -6,13 +6,11 @@ import GlassmorphismBox from '@/component/Second_page/glass_box';
 
 // Initial items for the DoubleLinkedList
 const initialItems = [
-  "#7C81AD",
-
-  "#AD7C9A",
-  
+  "#5f6497",
   "#ADA87C",
-  
-  "#7CAD90"
+  "#7CAD90",
+  "#907CAD",
+  "#7C9AAD",
 ];
 
 
@@ -31,23 +29,16 @@ const DoubleLinkedListLayout: React.FC = () => {
   };
 
   return (
-    <div className="h-screen flex gap-x-8 w-max-xl grid grid-rows-7 grid-cols-11 m-10 justify-center items-center">
+    <div className="h-screen flex gap-x-8 w-max-xl grid grid-rows-6 grid-cols-11 mx-10 mt-10 justify-center items-center">
 
-      <div className="flex flex-wrap col-start-7 col-end-12 row-start-1 row-end-7 m-2 justify-center items-end h-full w-full">
-      <div className={`m-4 bg-opacity-25 backdrop-blur-lg p-8 rounded-lg shadow-xl w-full h-full`} style={{ backgroundColor: visibleItems[Math.floor(visibleItems.length / 2)] }}>
-      <div className="text-gray-600">
-        Glassmorphism
-      </div>
-    </div>
+      <div className="flex flex-wrap col-start-1 col-end-3 row-start-2 row-end-5 m-2 justify-center items-end h-full w-full">
+        <GlassmorphismBox color={visibleItems[Math.floor(visibleItems.length / 2)]} item={"Glassmorphism"}/>
 
       </div>
+ 
 
 
-
-
-
-
-      <div className="row-start-7 h-1/2 col-span-full justify-center items-center mb-2">
+      <div className="row-start-6 h-1/2 col-span-full justify-center items-center mb-2">
         <div className="flex flex-row gap-x-8 p-2 bg-opacity-25 backdrop-blur-lg justify-center items-center">
           {/* DoubleLinkedList component */}
           <DoubleLinkedList items={initialItems} visibleItemCount={1} onVisibleItemsChange={handleVisibleItemsChange} />
--- portfolio/component/Second_page/glass_box.tsx
+++ portfolio/component/Second_page/glass_box.tsx
@@ -5,13 +5,37 @@ interface GlassmorphismBoxProps {
   color: string;
 }
 
-const GlassmorphismBox: React.FC<GlassmorphismBoxProps> = ({ item,color }) => {
+const GlassmorphismBox: React.FC<GlassmorphismBoxProps> = ({ item, color }) => {
   return (
-    <div className={`m-4 bg-opacity-25 backdrop-blur-lg bg-[${color}] p-8 rounded-lg shadow-xl w-full h-full`}>
-      <div className="text-gray-600">
-        {item}
+    <div
+    className='flex justify-center'
+      style={{
+        margin: '4px',
+        backdropFilter: 'blur(8px)',
+        padding: '32px',
+        borderRadius: '8px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        width: '100%',
+        height: '100%',
+        backgroundColor: color,
+      }}
+    >
+        <div className="max-w-sm rounded overflow-hidden shadow-lg justify-center">
+        <img className="w-28 border rounded-full border-white border-2 object-contain h-20 w-20" src="/photo.jpeg"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-center">
+            William Ho
+            </div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
       </div>
-      
     </div>
   );
 };
