0fc0275fdda5180b0f99b6ffe3a5b34fedb5c3ad
Will Ho <wkayi076@gmail.com>
2023-12-09 11:05:26 +0800

    edited dashboard visual

--- portfolio/component/dashboard/dashboard.tsx
+++ portfolio/component/dashboard/dashboard.tsx
@@ -6,7 +6,7 @@ import Example4 from '../tabs/skill/graphs/exampleline';
 
 const gridContainerStyle: React.CSSProperties = {
     display: 'grid',
-    gridTemplateRows: '1fr 1.25fr 2fr',
+    gridTemplateRows: '2fr 1fr',
     gap: '20px',
     height: '100%',
     width: '100%',
@@ -17,7 +17,7 @@ const gridContainerStyle: React.CSSProperties = {
 
 const grid1: React.CSSProperties = {
     display: 'grid',
-    gridTemplateColumns: '1fr 1fr 1fr',
+    gridTemplateColumns: '1fr 2fr',
     gap: '20px',
     height:"100%",
     width:"100%",
@@ -26,23 +26,15 @@ const grid1: React.CSSProperties = {
 
   const grid2: React.CSSProperties = {
     display: 'grid',
-    gridTemplateColumns: '1fr 1.5fr',
+    gridTemplateColumns: '1.5fr 1fr',
     gap: '20px',
     height:"100%",
     width:"100%",
 
   };
   
-  const grid3: React.CSSProperties = {
-    display: 'grid',
-    gridTemplateColumns: '2fr 1fr ',
-    gap: '20px',
-    height:"100%",
-    width:"100%",
 
-  };
-
-
+ 
 
 const gridItemStyle: React.CSSProperties = {
 
@@ -53,49 +45,49 @@ const gridItemStyle: React.CSSProperties = {
   alignItems:"center",
   color:"white",
   backgroundColor: "#476343",
-  maxHeight:"20em"
+
 
 };
+
+
+
 
 const Dashboard: React.FC = () => {
   return (
     <div style={gridContainerStyle}>
 
       <div style={grid1}>
-        <div style={gridItemStyle}>
+          <div style={gridItemStyle}>
+                <Example3/>
+            </div>
+        <div style={{display:"grid", gap:"10px"}}>
+          <div style={{display:"grid", gridTemplateColumns:"1fr 1fr", gap:"10px"}}>
+            <div style={gridItemStyle}>
             1st Place in Hackathon (Accenture 2022)
-        </div>
-        <div style={gridItemStyle}>
-        1st Place in Trading club competition
-        </div>
-        <div style={gridItemStyle}>
-        text
+            </div>
+            <div style={gridItemStyle}>
+            1st Place in Hackathon (Accenture 2022)
+            </div>
+            </div>
+            <div style={gridItemStyle}>
+                <Example/>
+            </div>
         </div>
       </div>
 
 
       <div style={grid2}>
       <div style={gridItemStyle}>
-            <Example3/>
-        </div>
-        <div style={gridItemStyle}>
-            <Example/>
-        </div>
-   
-      </div>
-
-
-      <div style={grid3}>
-        <div style={gridItemStyle}>
-        {/* <Example/> */}
+                <Example4/>
             </div>
         <div style={gridItemStyle}>
         This dashboard tracks the sales performance of a fictional bicycle manufacturer named AdventureWorks. It provides a great overview of total sales and growth over time with the ability to drill down by product category. Moreover, 
         it provides excellent comparison features between different years and months, alongside a great map of sales by region.  
         </div>
-     
-        </div>
- 
+      </div>
+
+
+  
 
 
     </div>
