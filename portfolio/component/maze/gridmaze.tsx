87b8fffd7ac03f4ffb15f6e4ca36c56605f9691c
Will Ho <wkayi076@gmail.com>
2023-12-11 00:09:46 +0800

    grid setup testing?

--- portfolio/component/maze/gridmaze.tsx
+++ portfolio/component/maze/gridmaze.tsx
@@ -7,27 +7,29 @@ const gridItemStyle: React.CSSProperties = {
   backgroundColor: '#476343',
   height: '4em',
   width: '4em',
-//   border: '1px solid #fff',
+  border: '1px solid #fff',
 };
 
 const column: React.CSSProperties = {
   display: 'grid',
   gridTemplateColumns: 'repeat(23, 1fr)',
 
-//   border: '1px solid #fff',
+  border: '1px solid #fff',
 };
 
 const row: React.CSSProperties = {
   display: 'grid',
   gridTemplateRows: 'repeat(12, 1fr)',
-//   border: '1px solid #fff',
+  border: '1px solid #fff',
 };
 
 interface GridState {
-  grid: string[][];
-}
+    grid: string[][];
+  }
+  
 
 class Grid extends React.Component<{}, GridState> {
+
   constructor(props: {}) {
     super(props);
 
@@ -35,7 +37,23 @@ class Grid extends React.Component<{}, GridState> {
     this.state = {
       grid: Array.from({ length: 12 }, () => Array(23).fill('.'))
     };
+
+
+ 
   }
+
+  fillgrid(row: number, col: number, startingpoint:{x:number, y:number}) {
+    // Create a 2D array representing the entire grid
+    const smallgrid = Array.from({ length: col }, (_, rowIndex) =>
+        Array(row).fill(null).map((_, colIndex) => 
+        ({ col: colIndex + startingpoint.x ,row: rowIndex + startingpoint.y  }))
+    );
+    // Flatten the 2D array to get a 1D array of coordinates
+    const flatSmallGrid = smallgrid.flat();
+    // Update the color for the specified coordinates
+    return flatSmallGrid
+  }
+  
 
   // Function to update color at specified coordinates
   updateColorAtCoordinates = (coordinates: { row: number; col: number }[], color: string) => {
@@ -53,17 +71,25 @@ class Grid extends React.Component<{}, GridState> {
     });
   };
 
+
   componentDidMount() {
     // Example: Change the color of cell at (2, 3) to 'red' after component mounts
-    this.updateColorAtCoordinates([{ row: 2, col: 3 }, {row: 0, col:0}], 'red');
+    const component1 = this.fillgrid(8,4, {x: 0,y:0})
+    this.updateColorAtCoordinates(component1,'red')
+    const component2= this.fillgrid(7, 1, {x: 9,y:0})
+    this.updateColorAtCoordinates(component2,'green')
+    const component3= this.fillgrid(6, 1, {x: 17,y:0})
+    this.updateColorAtCoordinates(component3,'green')
+ 
   }
+
   render() {
     return (
       <div style={row}>
         {this.state.grid.map((row, rowIndex) => (
           <div key={rowIndex} style={column}>
             {row.map((cell, colIndex) => (
-              <div key={colIndex} style={gridItemStyle}>
+              <div key={colIndex} style={{ ...gridItemStyle, backgroundColor: cell }}>
                 {cell}
               </div>
             ))}
@@ -72,6 +98,8 @@ class Grid extends React.Component<{}, GridState> {
       </div>
     );
   }
+  
+  
 }
 
 export default Grid;
